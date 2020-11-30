6a56349101e9d19bf20d715dbe673f378bf0c18c
BarrackHa <barakiey@gmail.com>
2020-11-30 14:27:55 +0700

    use my img with vim and stuff

--- runner.sh
+++ runner.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-docker run -it --rm -v /home/barak/Projects/google_chal/sol:/sol  --name python2.7  python:2.7.13 /bin/bash 
+docker run -it --rm -v /home/barak/Projects/google_chal/sol:/sol  --name python2.7  my_python2 /bin/bash 
 
