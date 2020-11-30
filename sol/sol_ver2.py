d6b31c25fb2cb32b974ecfa8a1990889e9680ecd
BarrackHa <barakiey@gmail.com>
2020-11-30 17:08:11 +0700

    added copy of sol ver2.

---
+++ sol/sol_ver2.py
@@ -0,0 +1,42 @@
+#! python2
+from math import sqrt
+
+primesLst = [2,3]
+primesStr = '23'
+	
+def isPrime(n):
+	for q in range(2,int(sqrt(n))+1):
+		if n%q == 0:
+			return False
+	return True	
+
+def getNextPrime():
+	global primesLst
+	thisP = primesLst[-1]
+	nextP = thisP + 2
+	while not isPrime(nextP):
+		nextP += 2
+	return nextP
+
+def genPrimesStr(i):
+	global primesStr
+	global primesLst
+	print '### ',len(primesStr)
+	while len(primesStr) < i+5:
+		p = getNextPrime()
+		primesLst.append(p)
+		primesStr = primesStr + str(p)
+	return primesStr
+
+
+def solution(i):
+	global primesLst
+	global primesStr
+	primes = genPrimesStr(i)
+	return primes[i:i+5]
+
+print solution(3)
+print solution(178)
+print solution(10000)
+print solution(18)
+print solution(99)
