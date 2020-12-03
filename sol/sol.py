bfbce9672999a8af5072cb05ead492a9014877da
BarrackHa <barakiey@gmail.com>
2020-12-03 20:55:50 +0700

    new O(n) sol.

--- sol/sol.py
+++ sol/sol.py
@@ -1,12 +1,17 @@
 def sol(s):
-    counter = 0
-    for i in range(len(s)): 
-        if s[i] == '>':
-            for d in s[i:]:
-                counter = counter+1 if d=='<' else counter
-    return 2*counter
+    rightWalkers = 0
+    salutes = 0
+    for c in s:
+        if c == '>':
+            rightWalkers += 1
+        elif c == '-':
+            continue
+        else:
+            salutes += 2 * rightWalkers
+    return salutes
 
 
 print(sol('>----<'))
 print(sol("<<>><"))
 print(sol("--->-><-><-->-"))
+print(sol('<<<<<<>>>>>>'))
