8f4fea81feb11d13b8517f0006f22c3eec3e40b1
BarrackHa <barakiey@gmail.com>
2020-12-03 20:54:19 +0700

    Added an example file builder for testing. Add a new O(n) solution. Added comp between sols.

--- ex3/helper.py
+++ ex3/helper.py
@@ -1,3 +1,5 @@
+from random import randint
+
 def sol(s):
     counter = 0
     for i in range(len(s)): 
@@ -6,7 +8,47 @@ def sol(s):
                 counter = counter+1 if d=='<' else counter
     return 2*counter
 
+def exampleFileBuilder(numOfExamplesToGen, newFile=False):
+    fileUsage = 'w' if newFile else 'a'
+    f = open('./ex3/examples.txt', fileUsage)
+    while(numOfExamplesToGen > 0):
+        strLen = randint(1,100)
+        tmpS = ''
+        for i in range(strLen):
+            tmpS += ['-','<','>'][randint(0,2)]
+        f.write(tmpS+'\n')
+        numOfExamplesToGen -= 1
+    f.close()
+    return
 
-print(sol('>----<'))
-print(sol("<<>><"))
-print(sol("--->-><-><-->-"))
+def sol3(s):
+    leftWalkers = 0
+    salutes = 0
+    for c in s:
+        if c == '>':
+            leftWalkers += 1
+        elif c == '-':
+            continue
+        else:
+            salutes += 2 * leftWalkers
+    return salutes
+
+#exampleFileBuilder(100000,newFile=True)
+#print(sol('>----<'))
+#print(sol("<<>><"))
+#print(sol("--->-><-><-->-"))
+#print(sol3('>----<'))
+#print(sol3("<<>><"))
+#print(sol3("--->-><-><-->-"))
+
+with open('./ex3/examples.txt','r') as f:
+    tot = 0
+    for l in f:
+        l = l.strip('\n')
+        if not (sol(l) == sol3(l)):
+            print(l)
+            tot += 1
+    print(tot, ' diffs found between sol and sol3')
+
+
+
